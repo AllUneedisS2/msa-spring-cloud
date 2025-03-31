712e5f1fdf5dc8f9363d5575f258e9b2a6e92e5f
AllUneedisS2 <bmdk594@naver.com>
2025-03-31 20:27:21 +0900

    20250331-2026

--- apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -2,8 +2,6 @@ package com.example.apigatewayservice.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 //@Component
 public class FilterConfig {
@@ -21,6 +19,5 @@ public class FilterConfig {
                              .uri("http://localhost:8082"))
                 .build();
     }
-
 
 }
