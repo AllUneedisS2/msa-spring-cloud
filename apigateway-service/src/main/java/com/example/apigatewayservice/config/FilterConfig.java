398e90bf1a8470ecb50be2c4dab2f06197023eb3
AllUneedisS2 <bmdk594@naver.com>
2025-04-11 09:45:15 +0900

    20250411-0945

--- apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -9,13 +9,13 @@ public class FilterConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
-                             .filters(f -> f.addRequestHeader("first-request", "first-request-header-by-java")
-                                            .addResponseHeader("first-response", "first-response-header-from-java"))
-                             .uri("http://localhost:8081"))
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header-by-java")
+                                .addResponseHeader("first-response", "first-response-header-from-java"))
+                        .uri("http://localhost:8081"))
                 .route(r -> r.path("/second-service/**")
-                             .filters(f -> f.addRequestHeader("second-request", "second-request-header-by-java")
-                                            .addResponseHeader("second-response", "second-response-header-from-java"))
-                             .uri("http://localhost:8082"))
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header-by-java")
+                                .addResponseHeader("second-response", "second-response-header-from-java"))
+                        .uri("http://localhost:8082"))
                 .build();
     }
 
