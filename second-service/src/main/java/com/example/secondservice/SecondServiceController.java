68325a90b796923f044d622a53b886220152307c
AllUneedisS2 <bmdk594@naver.com>
2025-04-28 13:04:37 +0900

    20250428-1304

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
-
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
@@ -21,5 +20,4 @@ public class SecondServiceController {
         log.info(header);
         return "Hello World in Second Service.";
     }
-
 }
