fb914311cb16ad24f18ab3d6c0a42567eca7da7c
AllUneedisS2 <bmdk594@naver.com>
2025-05-31 16:55:22 +0900

    20250531-1655

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -3,8 +3,8 @@ package com.example.apigatewayservice.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -17,7 +17,8 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
+        // Custom Pre Filter
+        return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
@@ -27,9 +28,10 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 log.info("Custom POST filter: response code -> {}", response.getStatusCode());
             }));
-        });
+        };
     }
 
-    public static class Config {}
-
+    public static class Config {
+        // Put the configuration properties
+    }
 }
