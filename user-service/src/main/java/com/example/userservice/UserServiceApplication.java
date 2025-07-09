f32d31b5179925521134f13febdb92ff397f4db2
AllUneedisS2 <bmdk594@naver.com>
2025-07-09 10:23:18 +0900

    20250709-1023

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -22,7 +22,6 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
-    
     @Bean
 //    @LoadBalanced
     public RestTemplate getRestTemplate() {
