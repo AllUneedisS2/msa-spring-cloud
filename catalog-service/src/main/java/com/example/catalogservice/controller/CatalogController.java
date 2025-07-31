5e5a399b27a1b812ccfab453d99956b7d0fbbdbb
AllUneedisS2 <bmdk594@naver.com>
2025-07-31 15:41:06 +0900

    20250731-1541

--- catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -5,8 +5,9 @@ import com.example.catalogservice.service.CatalogService;
 import com.example.catalogservice.vo.ResponseCatalog;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +24,8 @@ public class CatalogController {
     Environment env;
     CatalogService catalogService;
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
 
     @Autowired
     public CatalogController(Environment env, CatalogService catalogService) {
@@ -32,13 +33,13 @@ public class CatalogController {
         this.catalogService = catalogService;
     }
 
-    @GetMapping("/health_check")
+    @GetMapping("/health-check")
     public String status() {
-        List<ServiceInstance> serviceList = getApplications();
-        for (ServiceInstance instance : serviceList) {
-            System.out.println(String.format("instanceId:%s, serviceId:%s, host:%s, scheme:%s, uri:%s",
-                    instance.getInstanceId(), instance.getServiceId(), instance.getHost(), instance.getScheme(), instance.getUri()));
-        }
+//        List<ServiceInstance> serviceList = getApplications();
+//        for (ServiceInstance instance : serviceList) {
+//            System.out.println(String.format("instanceId:%s, serviceId:%s, host:%s, scheme:%s, uri:%s",
+//                    instance.getInstanceId(), instance.getServiceId(), instance.getHost(), instance.getScheme(), instance.getUri()));
+//        }
 
         return String.format("It's Working in Catalog Service on LOCAL PORT %s (SERVER PORT %s)",
                 env.getProperty("local.server.port"),
@@ -57,15 +58,15 @@ public class CatalogController {
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
-    private List<ServiceInstance> getApplications() {
-
-        List<String> services = this.discoveryClient.getServices();
-        List<ServiceInstance> instances = new ArrayList<ServiceInstance>();
-        services.forEach(serviceName -> {
-            this.discoveryClient.getInstances(serviceName).forEach(instance ->{
-                instances.add(instance);
-            });
-        });
-        return instances;
-    }
+//    private List<ServiceInstance> getApplications() {
+//
+//        List<String> services = this.discoveryClient.getServices();
+//        List<ServiceInstance> instances = new ArrayList<ServiceInstance>();
+//        services.forEach(serviceName -> {
+//            this.discoveryClient.getInstances(serviceName).forEach(instance ->{
+//                instances.add(instance);
+//            });
+//        });
+//        return instances;
+//    }
 }
